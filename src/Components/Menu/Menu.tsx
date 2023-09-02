e579c13e1ed1cdc835d5b1e72e2d407887605622
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-09-02 15:22:41 +0530

    fix: test issue

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,52 +1,56 @@
 import { FC } from 'react';
 import Pizza from '../Pizza/Pizza';
 import classes from 'classnames';
+import image1 from '../../assets/focaccia.jpg';
+import image2 from '../../assets/margherita.jpg';
+import image3 from '../../assets/spinaci.jpg';
+import image4 from '../../assets/funghi.jpg';
+import image5 from '../../assets/salamino.jpg';
+import image6 from '../../assets/prosciutto.jpg';
 
 const menuStyles = classes('flex flex-col items-center gap-[4rem]');
 const menuTextStyles = classes(
 	'inline-block py-4 border-t-[2px] border-b-[2px] border-solid border-current text-[2.4rem] uppercase tracking-[3px] font-medium',
 );
+const images = [image1, image2, image3, image4, image5, image6];
 const pizzaData = [
 	{
 		name: 'Focaccia',
 		ingredients: 'Bread with italian olive oil and rosemary',
 		price: 6,
-		photoName: 'focaccia.jpg',
 		soldOut: false,
 	},
 	{
 		name: 'Pizza Margherita',
 		ingredients: 'Tomato and mozarella',
 		price: 10,
-		photoName: 'margherita.jpg',
 		soldOut: false,
 	},
 	{
 		name: 'Pizza Spinaci',
 		ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
 		price: 12,
-		photoName: 'spinaci.jpg',
 		soldOut: false,
 	},
 	{
 		name: 'Pizza Funghi',
 		ingredients: 'Tomato, mozarella, mushrooms, and onion',
 		price: 12,
-		photoName: 'funghi.jpg',
+		photoName: '/assets/funghi.jpg',
 		soldOut: false,
 	},
 	{
 		name: 'Pizza Salamino',
 		ingredients: 'Tomato, mozarella, and pepperoni',
 		price: 15,
-		photoName: 'salamino.jpg',
+		photoName: '/assets/salamino.jpg',
 		soldOut: true,
 	},
 	{
 		name: 'Pizza Prosciutto',
 		ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
 		price: 18,
-		photoName: 'prosciutto.jpg',
+		photoName: '/assets/prosciutto.jpg',
 		soldOut: false,
 	},
 ];
@@ -59,12 +63,12 @@ const Menu: FC = () => {
 				our stone oven. All made with love.
 			</p>
 			<div className={'list-none grid gap-[4.8rem] grid-cols-2'}>
-				{pizzaData.map((pizza) => (
+				{pizzaData.map((pizza, index) => (
 					<Pizza
 						key={pizza.name}
 						name={pizza.name}
 						ingredients={pizza.ingredients}
-						imageUrl={pizza.photoName}
+						imageUrl={images[index]}
 						price={pizza.price}
 						soldOut={pizza.soldOut}
 					/>
